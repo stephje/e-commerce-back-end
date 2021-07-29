feb349fb564a04e719f3e14c00bee7b12708d953
stephje <s.jenkins3018@gmail.com>
2021-07-29 14:21:46 +0800

    Fixed error in 'through' property

--- models/index.js
+++ models/index.js
@@ -15,10 +15,10 @@ Category.hasMany(Product, {
 });
 
 // Products belongToMany Tags (through ProductTag)
-Product.belongsToMany(Tag, { through: 'ProductTag'});
+Product.belongsToMany(Tag, { through: ProductTag });
 
 // Tags belongToMany Products (through ProductTag)
-Tag.belongsToMany(Product, {through: 'ProductTag'});
+Tag.belongsToMany(Product, {through: ProductTag });
 
 module.exports = {
   Product,
