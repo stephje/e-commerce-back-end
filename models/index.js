a74edd63c281dcf9f844d14958f4ebe58f6c8a8b
stephje <s.jenkins3018@gmail.com>
2021-07-30 10:55:49 +0800

    Fixed typo in index.js file that was creating extra column

--- models/index.js
+++ models/index.js
@@ -6,12 +6,12 @@ const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
 Product.belongsTo(Category, {
-  foreignKey: 'category_id'
+  foreignKey: 'category_id',
 });
 
 // Categories have many Products
 Category.hasMany(Product, { 
-  foreignKey: 'cateogory_id'
+  foreignKey: 'category_id',
 });
 
 // Products belongToMany Tags (through ProductTag)
